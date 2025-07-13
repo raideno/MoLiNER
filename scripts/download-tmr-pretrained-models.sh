2ba002ed268facd9e94327c7ab60d747556a2a97
raideno56 <nadirkichou@hotmail.fr>
2025-07-13 17:12:52 +0200

    chore: improve script code & checksum

--- scripts/download-tmr-pretrained-models.sh
+++ scripts/download-tmr-pretrained-models.sh
@@ -1,21 +1,44 @@
 #!/bin/bash
 
-# NOTE: bash prepare/download-tmr-pretrained-models.sh
+echo "[download-tmr]: Setting up TMR pretrained models..."
 
-echo -e "The pretrained models will stored in the 'models' folder\n"
-mkdir -p models
+echo "[download-tmr]: Creating models directory..."
+if test -d models; then
+    echo "[download-tmr]: Models directory already exists."
+else
+    mkdir -p models
+    echo "[download-tmr]: Models directory created."
+fi
+
+echo "[download-tmr]: Downloading pretrained models from Google Drive..."
 python -m gdown "https://drive.google.com/uc?id=1n6kRb-d2gKsk8EXfFULFIpaUKYcnaYmm"
 
-echo -e "Please check that the md5sum is: 7b6d8814f9c1ca972f62852ebb6c7a6f"
-echo -e "+ md5sum tmr_models.tgz"
-md5sum tmr_models.tgz
+if test -f tmr_models.tgz; then
+    echo "[download-tmr]: Download completed successfully."
+else
+    echo "[download-tmr]: Download failed. Please check your internet connection and try again."
+    exit 1
+fi
 
-echo -e "If it is not, please rerun this script"
+echo "[download-tmr]: Verifying file integrity with MD5 checksum..."
+EXPECTED_MD5="7b6d8814f9c1ca972f62852ebb6c7a6f"
+ACTUAL_MD5=$(md5sum tmr_models.tgz | cut -d' ' -f1)
 
-sleep 5
+if [ "$ACTUAL_MD5" = "$EXPECTED_MD5" ]; then
+    echo "[download-tmr]: MD5 checksum verification passed."
+else
+    echo "[download-tmr]: MD5 checksum verification failed!"
+    echo "[download-tmr]: Expected: $EXPECTED_MD5"
+    echo "[download-tmr]: Actual: $ACTUAL_MD5"
+    echo "[download-tmr]: Please rerun this script to download the file again."
+    rm tmr_models.tgz
+    exit 1
+fi
+
+echo "[download-tmr]: Extracting models..."
 tar xfzv tmr_models.tgz
 
-echo -e "Cleaning\n"
+echo "[download-tmr]: Cleaning up temporary files..."
 rm tmr_models.tgz
 
-echo -e "Downloading done!"
+echo "[download-tmr]: TMR pretrained models setup complete!"
