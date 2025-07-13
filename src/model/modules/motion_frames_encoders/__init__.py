e82b2186f12ff81741fedbdf7e6a590843b4b4bb
raideno56 <nadirkichou@hotmail.fr>
2025-07-13 22:25:33 +0200

    feat: implement mlp and lstm based motion frame encoder

--- src/model/modules/motion_frames_encoders/__init__.py
+++ src/model/modules/motion_frames_encoders/__init__.py
@@ -1,3 +1,6 @@
 from ._base import BaseMotionFramesEncoder
 
 from .tmr import TMRMotionFramesEncoder
+from .mlp import MLPMotionFramesEncoder
+from .lstm import LSTMMotionFramesEncoder
+from .temporal_gnn import TemporalGNNMotionFramesEncoder
