9eee6416bbf25b999c2165dc9b0ab73d2f92161e
raideno56 <nadirkichou@hotmail.fr>
2025-07-13 16:13:30 +0200

    feat: add script to easily get started

--- scripts/port-forwarding-example.sh
+++ scripts/port-forwarding-example.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # NOTE: this is to forward the port 7860 from a remote server to the local machine and thus be able to access the Gradio interface running on that port.
 # The -N option is used to use the connection only for port forwarding, no shell access.
 ssh -N -L 7860:localhost:7860 nadir@10.59.129.243
