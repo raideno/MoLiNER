21609169c071bdedce08af2fcbaf6d3c152e3d59
Nadir Kichou <nadirkichou@hotmail.fr>
2025-07-30 10:57:00 +0200

    feat: optimize convolution span representation layer

--- src/model/modules/span_representation_layers/convolution.py
+++ src/model/modules/span_representation_layers/convolution.py
@@ -1,5 +1,5 @@
+import pdb
 import torch
-import torch.nn as nn
 import typing
 
 from src.model.modules._helpers import create_projection_layer
@@ -7,36 +7,55 @@ from src.model.modules._helpers import create_projection_layer
 from ._base import BaseSpanRepresentationLayer
 
 class SpanConvBlock(torch.nn.Module):
-    def __init__(self, hidden_size: int, kernel_size: int, conv_mode: str = 'conv'):
+    def __init__(
+        self,
+        hidden_size: int,
+        kernel_size: int,
+        mode: str
+    ):
         super().__init__()
         
+        self.mode = mode
+        self.hidden_size = hidden_size
         self.kernel_size = kernel_size
-        self.conv_mode = conv_mode
         
-        if conv_mode == 'conv':
-            self.conv = torch.nn.Conv1d(hidden_size, hidden_size, kernel_size=kernel_size)
-            torch.nn.init.kaiming_uniform_(self.conv.weight, nonlinearity='relu')
-        elif conv_mode == 'max':
-            self.conv = torch.nn.MaxPool1d(kernel_size=kernel_size, stride=1)
-        elif conv_mode in ['mean', 'sum']:
-            self.conv = torch.nn.AvgPool1d(kernel_size=kernel_size, stride=1)
-        else:
-            raise ValueError(f"Unknown conv_mode: {conv_mode}")
+        self.convolution = self._setup_convolution_layer(self.mode)
         
-        self.pad = kernel_size - 1
+        self.padding = kernel_size - 1
 
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
+    def forward(
+        self,
+        x: torch.Tensor
+    ) -> torch.Tensor:
         x = torch.einsum('bld->bdl', x)
         
-        if self.pad > 0:
-            x = torch.nn.functional.pad(x, (0, self.pad), "constant", 0)
+        # NOTE: ensures we have the same sequence length after convolution
+        if self.padding > 0:
+            x = torch.nn.functional.pad(x, (0, self.padding), "constant", 0)
         
-        x = self.conv(x)
+        x = self.convolution(x)
         
-        if self.conv_mode == "sum":
-            x = x * (self.pad + 1)
+        if self.mode == "sum":
+            x = x * (self.padding + 1)
         
         return torch.einsum('bdl->bld', x)
+    
+    def _setup_convolution_layer(
+        self,
+        convolution_mode: str
+    ) -> torch.nn.Module:
+        if convolution_mode == 'convolution':
+            self.convolution = torch.nn.Conv1d(self.hidden_size, self.hidden_size, kernel_size=self.kernel_size)
+            torch.nn.init.kaiming_uniform_(self.convolution.weight, nonlinearity='relu')
+        elif convolution_mode == 'max':
+            self.convolution = torch.nn.MaxPool1d(kernel_size=self.kernel_size, stride=1)
+        elif convolution_mode in ['mean', 'sum']:
+            self.convolution = torch.nn.AvgPool1d(kernel_size=self.kernel_size, stride=1)
+        else:
+            raise ValueError(f"Unknown convolution_mode: {convolution_mode}")
+        
+        return self.convolution
+        
 
 class ConvolutionalSpanRepresentationLayer(BaseSpanRepresentationLayer):
     def __init__(
@@ -45,9 +64,9 @@ class ConvolutionalSpanRepresentationLayer(BaseSpanRepresentationLayer):
         representation_dim: int, 
         min_span_width: int,
         max_span_width: int,
-        conv_mode: str,
+        step: int,
+        mode: str,
         dropout: float,
-        step: int
     ):
         """
         Args:
@@ -55,9 +74,9 @@ class ConvolutionalSpanRepresentationLayer(BaseSpanRepresentationLayer):
             representation_dim (int): The dimension of the final output representation.
             min_span_width (int): Minimum span width to support.
             max_span_width (int): Maximum span width to support.
-            conv_mode (str): Type of convolution operation ('conv', 'max', 'mean', 'sum').
-            dropout (float): The dropout rate to apply for regularization.
             step (int): Step size for generating kernel sizes.
+            mode (str): Type of convolution operation ('convolution', 'max', 'mean', 'sum').
+            dropout (float): The dropout rate to apply for regularization.
         """
         super().__init__()
         
@@ -65,24 +84,25 @@ class ConvolutionalSpanRepresentationLayer(BaseSpanRepresentationLayer):
         self.representation_dim = representation_dim
         self.max_span_width = max_span_width
         self.min_span_width = min_span_width
-        self.conv_mode = conv_mode
+        self.mode = mode
         self.step = step
         
         self.kernels = list(range(min_span_width, max_span_width + 1, step))
         
-        self.conv_blocks = torch.nn.ModuleList()
+        # type: ignore
+        self.convolution_blocks: typing.List[torch.nn.Module] = torch.nn.ModuleList()
         for kernel_size in self.kernels:
-            self.conv_blocks.append(
-                SpanConvBlock(motion_embed_dim, kernel_size, conv_mode)
+            self.convolution_blocks.append(
+                SpanConvBlock(motion_embed_dim, kernel_size, mode)
             )
         
         # self.output_projection = create_projection_layer(
         #     motion_embed_dim, dropout, representation_dim
         # )
         
-        self.output_projection = nn.Sequential(
-            nn.ReLU(),
-            nn.Linear(motion_embed_dim, representation_dim)
+        self.output_projection = torch.nn.Sequential(
+            torch.nn.ReLU(),
+            torch.nn.Linear(motion_embed_dim, representation_dim)
         )
 
     def forward(
@@ -104,69 +124,44 @@ class ConvolutionalSpanRepresentationLayer(BaseSpanRepresentationLayer):
         # span_reps = [x]
         span_reps = []
         
-        for conv_block in self.conv_blocks:
+        # NOTE: we execute each convolution block independently over the motion features, for each block / kernel size of width w
+        # and given the motion features of shape (batch_size, seq_len, embed_dim), we get the output of shape (batch_size, seq_len, embed_dim).
+        # NOTE: so now we have the span representation of every possible width w in the range [min_span_width, max_span_width] for each motion feature.
+        for convolution_block in self.convolution_blocks:
             # NOTE: h: (batch_size, seq_len, embed_dim)
-            h = conv_block(motion_features)
-            span_reps.append(h)
-        
-        # stacked_features = torch.stack(span_reps, dim=-2)
+            span_reps.append(convolution_block.forward(motion_features))
+
         # NOTE: stacked_features: (batch_size, seq_len, num_widths, embed_dim)
-        stacked_features = torch.stack(span_reps, dim=2)
+        stacked_features = torch.stack(span_reps, dim=-2)
+        all_span_reps = self.output_projection(stacked_features)
         
-        span_representations = self._extract_span_representations(
-            stacked_features,
-            span_indices,
-            spans_masks,
-            # self.min_span_width,
-            # self.max_span_width
-        )
-        
-        output = self.output_projection(span_representations)
-        
-        masked_output = output * spans_masks.unsqueeze(-1)
-        
-        return masked_output
-    
-    def _extract_span_representations(
-        self,
-        stacked_features: torch.Tensor,
+        if span_indices is not None:
+            return self._extract_specific_spans(all_span_reps, span_indices, spans_masks)
+        else:
+            return all_span_reps
+
+    def _extract_specific_spans(
+        self, 
+        all_span_reps: torch.Tensor,
         span_indices: torch.Tensor,
         spans_masks: torch.Tensor
     ) -> torch.Tensor:
-        """
-        Args:
-            stacked_features: (batch_size, seq_len, num_widths, embed_dim)
-            span_indices: (batch_size, max_spans, 2)
-            spans_masks: (batch_size, max_spans)
-            
-        Returns:
-            torch.Tensor: (batch_size, max_spans, embed_dim)
-        """
-        batch_size, seq_len, num_widths, embed_dim = stacked_features.shape
+        batch_size, seq_len, max_width, hidden_size = all_span_reps.shape
         max_spans = span_indices.shape[1]
         
-        span_representations = []
+        # NOTE: used to index batch dimension and get the correct span representations
+        batch_idx = torch.arange(batch_size, device=all_span_reps.device).unsqueeze(1).expand(-1, max_spans)
         
-        for batch_idx in range(batch_size):
-            batch_span_reps = []
-            
-            for span_idx in range(max_spans):
-                if not spans_masks[batch_idx, span_idx]:
-                    # NOTE: masked span, serves as padding
-                    span_rep = torch.zeros(embed_dim, device=stacked_features.device)
-                else:
-                    start_idx = span_indices[batch_idx, span_idx, 0].item()
-                    end_idx = span_indices[batch_idx, span_idx, 1].item()
-                    
-                    span_width = end_idx - start_idx + 1
-                    
-                    # NOTE: given the span width, we determine the corresponding conv block width
-                    width_idx = (span_width - self.min_span_width) // self.step
-                    
-                    span_rep = stacked_features[batch_idx, start_idx, width_idx, :]
-                
-                batch_span_reps.append(span_rep)
-            
-            span_representations.append(torch.stack(batch_span_reps))
+        start_indices = span_indices[:, :, 0]
+        end_indices = span_indices[:, :, 1]
+        # NOTE: widths = end_indices - start_indices + 1, clamping to ensure we don't exceed max_width
+        # widths are used to index the correct span representations
+        widths = torch.clamp(end_indices - start_indices + 1, min=1, max=max_width) - 1
+        # widths = torch.clamp(end_indices - start_indices + 1, min=0, max=max_width-1)
         
-        return torch.stack(span_representations)
+        # NOTE: we index using start_indices since its the place where the kernel store its results
+        result = all_span_reps[batch_idx, start_indices, widths]
+        
+        result = result * spans_masks.unsqueeze(-1)
+        
+        return result
